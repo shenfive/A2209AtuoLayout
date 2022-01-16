feee00c37a901009cb1989d38d054d089ffe73a6
Danny Shen <shenfive@gmail.com>
2022-01-16 15:47:50 +0800

    Coment 1

--- A2209AtuoLayout/ViewController.swift
+++ A2209AtuoLayout/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let userDef = UserDefaults.standard
+        let date = userDef.object(forKey: "lastInput")
+        if let theDate = date as? Date {
+            let dateFormater = DateFormatter()
+            dateFormater.dateFormat = "yyyy/MMMM/dd HH:mm"
+            dateFormater.locale = Locale(identifier: "zh_Hant_TW")
+            let dateString = dateFormater.string(from: theDate)
+            displayLabel.text = dateString
+        }else{
+            displayLabel.text = ""
+        }
     }
 
-
+    @IBAction func selectDateAction(_ sender: UIDatePicker) {
+        let theDate = sender.date
+        let userDef = UserDefaults.standard
+        userDef.set(theDate, forKey: "lastInput")
+        userDef.synchronize()
+        
+        
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "yyyy/MMMM/dd HH:mm"
+        dateFormater.locale = Locale(identifier: "zh_Hant_TW")
+        let dateString = dateFormater.string(from: theDate)
+        displayLabel.text = dateString
+        
+        
+        
+    }
+    
 }
 
