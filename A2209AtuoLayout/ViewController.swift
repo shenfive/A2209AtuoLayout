54947e94d4a7825bd124cf1af3539a0a4e92e11c
Danny Shen <shenfive@gmail.com>
2022-01-16 13:14:50 +0800

    Initial Commit

---
+++ A2209AtuoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2209AtuoLayout
+//
+//  Created by 申潤五 on 2022/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
